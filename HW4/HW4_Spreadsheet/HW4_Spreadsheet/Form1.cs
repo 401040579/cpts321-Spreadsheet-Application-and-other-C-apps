abbbb97ecdb17bff71e298e0e207fa26ace63c11
Ran Tao <ryantao96@gmail.com>
2018-06-15 11:04:19 -0700

    Add files via upload

---
+++ HW4/HW4_Spreadsheet/HW4_Spreadsheet/Form1.cs
@@ -0,0 +1,148 @@
+﻿/*******************************************************************************************
+* Programmer: Ran Tao
+* ID: 11488080
+* Class: CptS 321, Spring  2017
+* Programming Assignment: PA 4 - First Steps for your Spreadsheet Application
+* Created: Feb 8, 2017
+* Last Revised: Feb 9, 2017
+* Cited: MSDN documentation
+*******************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpreadsheetEngine;
+
+namespace HW4_Spreadsheet
+{
+    public partial class Form1 : Form
+    {
+        private Spreadsheet ssheet = new Spreadsheet(50, 26);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*************************************************************
+        * Function: Form1_Load(object sender, EventArgs e)
+        * Date Created: Feb 8, 2017
+        * Date Last Modified: Feb 9, 2017
+        * Description: load form
+        * Return: void
+        *************************************************************/
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ssheet.CellPropertyChanged += OnCellPropertyChanged;
+            dataGridView1.CellBeginEdit += dataGridView1_CellBeginEdit;
+            dataGridView1.CellEndEdit += dataGridView1_CellEndEdit;
+
+            dataGridView1.Columns.Clear();
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (int i = 0; i <= 25; i++)
+                dataGridView1.Columns.Add(letters[i].ToString(), letters[i].ToString());
+            dataGridView1.Rows.Add(50); //add 50 rows
+            for (int i = 0; i < 50; i++) //signing name 1-50
+            {
+                var row = dataGridView1.Rows[i];
+                row.HeaderCell.Value = (i + 1).ToString();
+            }
+        }
+
+        /*************************************************************
+        * Function: OnCellPropertyChanged(object sender, PropertyChangedEventArgs e)
+        * Date Created: Feb 8, 2017
+        * Date Last Modified: Feb 9, 2017
+        * Description: OnCellPropertyChanged
+        * Return: void
+        *************************************************************/
+        private void OnCellPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Cell selectedCell = sender as Cell;
+
+            if (e.PropertyName == "Value" && selectedCell != null)
+            {
+                dataGridView1.Rows[selectedCell.RowIndex].Cells[selectedCell.ColumnIndex].Value = selectedCell.Value;
+            }
+        }
+
+        /*************************************************************
+        * Function: dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        * Date Created: Feb 8, 2017
+        * Date Last Modified: Feb 9, 2017
+        * Description: dataGridView1_CellBeginEdit
+        * Return: void
+        *************************************************************/
+        void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            int row = e.RowIndex, column = e.ColumnIndex;
+
+            Cell spreadsheetCell = ssheet.GetCell(row, column);
+
+            dataGridView1.Rows[row].Cells[column].Value = spreadsheetCell.Text;
+        }
+
+        /*************************************************************
+        * Function: dataGridView1_CellEndEdit(object sender, DataGridViewCellCancelEventArgs e)
+        * Date Created: Feb 8, 2017
+        * Date Last Modified: Feb 9, 2017
+        * Description: dataGridView1_CellEndEdit
+        * Return: void
+        *************************************************************/
+        void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = e.RowIndex, column = e.ColumnIndex;
+            string m_Text;
+
+            Cell spreadsheetCell = ssheet.GetCell(row, column);     
+
+            try
+            {
+                m_Text = dataGridView1.Rows[row].Cells[column].Value.ToString();  
+            }
+            catch (NullReferenceException)
+            {
+                m_Text = "";
+            }
+
+            spreadsheetCell.Text = m_Text;     
+
+            dataGridView1.Rows[row].Cells[column].Value = spreadsheetCell.Value; 
+
+        }
+
+        /*************************************************************
+        * Function: button1_Click(object sender, DataGridViewCellCancelEventArgs e)
+        * Date Created: Feb 8, 2017
+        * Date Last Modified: Feb 9, 2017
+        * Description: button for demo
+        * Return: void
+        *************************************************************/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            /*The demo should set the text in about 50 random cells to a text string of your choice. 
+             * “Hello World!” would be fine or some other message would be ok too.*/
+            for (int i = 0; i < 50; i++)
+            {
+                int row = rnd.Next(0, 49);
+                int column = rnd.Next(2, 25); // columns start from >2 so nothing conflicts
+
+                ssheet.array[row, column].Text = "Hello World!";
+            }
+            /*Also, do a loop to set the text in every cell in column B to “This is cell B#”,
+             *  where # number is the row number for the cell.*/
+            for (int i = 0; i < 50; i++)
+            {
+                ssheet.array[i, 1].Text = "This is cell B" + (i + 1).ToString();
+            }
+            /*The result should be that the cells in column A update to have the same values as column B.*/
+            for (int i = 0; i < 50; i++) { ssheet.array[i, 0].Text = "=B" + (i + 1).ToString(); }
+        }
+    }
+}
---
+++ HW4/HW4_Spreadsheet/SpreadsheetEngine/Class1.cs
@@ -0,0 +1,206 @@
+﻿/*******************************************************************************************
+* Programmer: Ran Tao
+* ID: 11488080
+* Class: CptS 321, Spring  2017
+* Programming Assignment: PA 4 - First Steps for your Spreadsheet Application
+* Created: Feb 8, 2017
+* Last Revised: Feb 9, 2017
+* Cited: MSDN documentation
+*******************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetEngine
+{
+    public class Class1
+    {
+    }
+
+    public abstract class Cell : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private readonly int m_RowIndex, m_ColumnIndex;
+        protected string m_Text = "", m_Value = "";
+
+        /*************************************************************
+        * Function: Cell(int RowIndex, int ColumnIndex)
+        * Date Created: Feb 8, 2017
+        * Date Last Modified: Feb 9, 2017
+        * Description: Cell
+        * Return: NONE
+        *************************************************************/
+        public Cell(int RowIndex, int ColumnIndex)
+        {
+            m_RowIndex = RowIndex;
+            m_ColumnIndex = ColumnIndex;
+        }
+
+        public int RowIndex
+        {
+            get
+            {
+                return m_RowIndex;
+            }
+        }
+
+        public int ColumnIndex
+        {
+            get
+            {
+                return m_ColumnIndex;
+            }
+        }
+        public string Value
+        {
+            get
+            {
+                return m_Value;
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return m_Text;
+            }
+            set
+            {
+                if(m_Text == value)
+                {
+                    return;
+                }
+                m_Text = value;
+
+                PropertyChanged(this, new PropertyChangedEventArgs("Text"));
+            }
+        }
+    }
+
+    public class Spreadsheet
+    {
+        /*come up with a design here that actually allows the spreadsheet 
+         * to create cells*/
+        private class createACell : Cell
+        {
+            public createACell(int RowIndex, int ColumnIndex) : base(RowIndex, ColumnIndex) { }
+            
+            /*Value property is a getter only and you’ll have to implement a 
+            * way to allow the spreadsheet class to set the value, but no 
+            * other class can.*/
+            public void SetValue(string value)
+            {
+                m_Value = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler CellPropertyChanged;
+
+        /*************************************************************
+        * Function: Eval(Cell cell)
+        * Date Created: Feb 8, 2017
+        * Date Last Modified: Feb 9, 2017
+        * Description: eval
+        * Return: void
+        *************************************************************/
+        private void Eval(Cell cell)
+        {
+            createACell m_Cell = cell as createACell;
+
+            if(string.IsNullOrEmpty(m_Cell.Text))
+            {
+                m_Cell.SetValue("");
+                CellPropertyChanged(cell, new PropertyChangedEventArgs("Value"));
+
+            }
+            else if(m_Cell.Text[0] == '=' && m_Cell.Text.Length >= 3)
+            {
+                string letter = m_Cell.Text.Substring(1);
+                string number = letter.Substring(1);
+                string lettersAZ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                int col = 0, row = int.Parse(number) - 1;
+
+                for(int i = 0;i<26;i++)
+                {
+                    if (lettersAZ[i] == letter[0])
+                    {
+                        col = i;
+                        break;
+                    }
+                }
+                
+                m_Cell.SetValue(GetCell(row, col).Text);
+                CellPropertyChanged(cell, new PropertyChangedEventArgs("Value"));
+            }
+            else
+            {
+                m_Cell.SetValue(m_Cell.Text);
+                CellPropertyChanged(cell, new PropertyChangedEventArgs("Value"));
+            }
+        }
+
+        
+        public Cell[,] array;
+
+        /*************************************************************
+        * Function: Spreadsheet(int RowIndex, int ColumnIndex)
+        * Date Created: Feb 8, 2017
+        * Date Last Modified: Feb 9, 2017
+        * Description: Spreadsheet cons
+        * Return: NONE
+        *************************************************************/
+        public Spreadsheet(int RowIndex, int ColumnIndex)
+        {
+            array = new Cell[RowIndex, ColumnIndex];
+            for (int x = 0; x < RowIndex; x++)
+            {
+                for (int y = 0; y < ColumnIndex; y++)
+                {
+                    array[x, y] = new createACell(x, y);
+                    array[x, y].PropertyChanged += OnPropertyChanged;
+                }
+            }
+        }
+
+        /*************************************************************
+        * Function: OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        * Date Created: Feb 8, 2017
+        * Date Last Modified: Feb 9, 2017
+        * Description: OnPropertyChanged
+        * Return: void
+        *************************************************************/
+        public void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Text")
+            {
+                createACell tmpCell = sender as createACell;
+                
+                Eval(sender as Cell);
+            }
+        }
+        public Cell GetCell(int RowIndex, int ColumnIndex)
+        {
+            return array[RowIndex, ColumnIndex];
+        }
+        public int RowCount
+        {
+            get
+            {
+                return array.GetLength(0);
+            }
+        }
+
+        public int ColumnCount
+        {
+            get
+            {
+                return array.GetLength(1);
+            }
+        }
+
+    }
+}
