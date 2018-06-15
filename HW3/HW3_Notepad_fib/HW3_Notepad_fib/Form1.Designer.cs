6acebf45ff0c520ee4f5c4f42ff8e6f4bd3c0151
Ran Tao <ryantao96@gmail.com>
2018-06-15 11:03:51 -0700

    Add files via upload

---
+++ HW3/HW3_Notepad_fib/HW3_Notepad_fib/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace HW3_Notepad_fib
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFromFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(422, 446);
+            this.textBox1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(422, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFromFileToolStripMenuItem,
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem,
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem,
+            this.saveToFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadFromFileToolStripMenuItem
+            // 
+            this.loadFromFileToolStripMenuItem.Name = "loadFromFileToolStripMenuItem";
+            this.loadFromFileToolStripMenuItem.Size = new System.Drawing.Size(277, 22);
+            this.loadFromFileToolStripMenuItem.Text = "Load from file...";
+            this.loadFromFileToolStripMenuItem.Click += new System.EventHandler(this.loadFromFileToolStripMenuItem_Click);
+            // 
+            // loadFibonacciNumbersfirst50ToolStripMenuItem
+            // 
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem.Name = "loadFibonacciNumbersfirst50ToolStripMenuItem";
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem.Size = new System.Drawing.Size(277, 22);
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem.Text = "Load Fibonacci numbers (first 50)";
+            this.loadFibonacciNumbersfirst50ToolStripMenuItem.Click += new System.EventHandler(this.loadFibonacciNumbersfirst50ToolStripMenuItem_Click);
+            // 
+            // loadFibonacciNumbersfirst100ToolStripMenuItem
+            // 
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem.Name = "loadFibonacciNumbersfirst100ToolStripMenuItem";
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem.Size = new System.Drawing.Size(277, 22);
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem.Text = "Load Fibonacci numbers (first 100)";
+            this.loadFibonacciNumbersfirst100ToolStripMenuItem.Click += new System.EventHandler(this.loadFibonacciNumbersfirst100ToolStripMenuItem_Click);
+            // 
+            // saveToFileToolStripMenuItem
+            // 
+            this.saveToFileToolStripMenuItem.Name = "saveToFileToolStripMenuItem";
+            this.saveToFileToolStripMenuItem.Size = new System.Drawing.Size(277, 22);
+            this.saveToFileToolStripMenuItem.Text = "Save to file";
+            this.saveToFileToolStripMenuItem.Click += new System.EventHandler(this.saveToFileToolStripMenuItem_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(277, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 471);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Ran Tao 11488080 HW3";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFromFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFibonacciNumbersfirst50ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFibonacciNumbersfirst100ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToFileToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ HW3/HW3_Notepad_fib/HW3_Notepad_fib/Form1.cs
@@ -0,0 +1,195 @@
+﻿/*******************************************************************************************
+* Programmer: Ran Tao
+* ID: 11488080
+* Class: CptS 321, Spring  2017
+* Programming Assignment: PA 3 - WinForms “Notepad” Application / Fibonacci BigInt Text Reader
+* Created: Feb 1, 2017
+* Last Revised: Feb 1, 2017
+* Cited: MSDN documentation
+*******************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW3_Notepad_fib
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*************************************************************
+        * Function: exitToolStripMenuItem_Click(object sender, EventArgs e)
+        * Date Created: Feb 1, 2017
+        * Date Last Modified: Feb 1, 2017
+        * Description: exit the app
+        * Return: void
+        *************************************************************/
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //use OpenFileDialog/SaveFileDialog for selecting files to open/save
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        //use OpenFileDialog/SaveFileDialog for selecting files to open/save
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        /*************************************************************
+        * Function: saveToFileToolStripMenuItem_Click(object sender, EventArgs e)
+        * Date Created: Feb 1, 2017
+        * Date Last Modified: Feb 1, 2017
+        * Description: application needs to provide the ability to save 
+        * the text to a text file. Provide a button or menu option that 
+        * brings up a SaveFileDialog, prompting the user for a file name. 
+        * If the user clicks OK in that dialog, which you’ll determine by 
+        * checking the return value of the SaveFileDialog.ShowDialog 
+        * function, then save the text in the text box to that file. 
+        * You can save it using streams, text-writers, or any other 
+        * method you see fit (it’s the loading that’s going to be more 
+        * specific).
+        * Return: void
+        *************************************************************/
+        private void saveToFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamWriter myStream = null;
+            //SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                myStream = new StreamWriter(saveFileDialog1.FileName);
+                myStream.Write(textBox1.Text);
+                myStream.Close();
+            }
+        }
+        /*************************************************************
+        * Function: loadFromFileToolStripMenuItem_Click(object sender, EventArgs e)
+        * Date Created: Feb 1, 2017
+        * Date Last Modified: Feb 1, 2017
+        * Description: Add the option to load text from a file. You can 
+        * use a stream reader to open the file and pass it to your 
+        * loading function. Remember that StreamReader inherits from 
+        * TextReader and has a constructor that takes a file name as a parameter.
+        * Return: void
+        *************************************************************/
+        private void loadFromFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamReader myStream = null;
+            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                myStream = new StreamReader(openFileDialog1.FileName);
+                LoadText(myStream);
+                myStream.Close();
+            }
+        }
+        /*************************************************************
+        * Function: LoadText(TextReader sr)
+        * Date Created: Feb 1, 2017
+        * Date Last Modified: Feb 1, 2017
+        * Description: Use either ReadToEnd or ReadLine (in a loop) to 
+        * load all text from the reader and put it in the text box. 
+        * This should replace any content already in the text box.
+        * Return: void
+        *************************************************************/
+        private void LoadText(TextReader sr)
+        {
+            textBox1.Text = sr.ReadToEnd();
+        }
+        /*************************************************************
+        * Class: FibonacciTextReader
+        * Date Created: Feb 1, 2017
+        * Date Last Modified: Feb 1, 2017
+        * Description: Write a class named FibonacciTextReader that 
+        * inherits from the System.IO.TextReader.
+        *************************************************************/
+        class FibonacciTextReader : TextReader //Inherit from the TextReader class
+        {
+            int _maxNumber = 0, count = 0; //count is for print first 2's fib numbers.
+            BigInteger firstFib = 0, SecondFib = 1;
+            
+           /* Make it have a constructor that takes an integer as a parameter indicating the maximum
+            * number of lines available (the maximum number of numbers in the sequence that you
+            * can generate).*/
+            public FibonacciTextReader(int maxNumber)
+            {
+                _maxNumber = maxNumber;
+            }
+
+           /* Override the ReadLine method which delivers the next number (as a string) in the
+            * Fibonaci sequence. You need logic in this function to handle the first two numbers as
+            * special cases. You must return null after the nth call, where n is the integer value passed
+            * to the constructor.*/
+            public override string ReadLine()
+            {
+                BigInteger number;
+
+                if(count == 0)
+                {
+                    count++;
+                    return "0";
+                }
+                else if(count == 1)
+                {
+                    count++;
+                    return "1";
+                }
+                else
+                {
+                    number = firstFib + SecondFib;
+                    firstFib = SecondFib;
+                    SecondFib = number;
+                    return number.ToString(); //after the nth call, we don't need return anything.
+                }
+            }
+            /* Implement it such that it repeatedly calls ReadLine and 
+             * concatenates all the lines together.*/
+            public override string ReadToEnd()
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for(int i = 0; i < _maxNumber;i++)
+                {
+                    sb.Append(i + 1).Append(": ").AppendLine(ReadLine());
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        //load the first 50 numbers of the Fibonacci sequence
+        private void loadFibonacciNumbersfirst50ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FibonacciTextReader sr = new FibonacciTextReader(50);
+            textBox1.Text = sr.ReadToEnd();
+        }
+
+        //load the first 100 numbers of the Fibonacci sequence
+        private void loadFibonacciNumbersfirst100ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FibonacciTextReader sr = new FibonacciTextReader(100);
+            textBox1.Text = sr.ReadToEnd();
+        }
+    }
+}
