415953cfd262d29f15a022239e11e73ea462d705
Ran Tao <ryantao96@gmail.com>
2018-06-15 11:03:12 -0700

    Add files via upload

---
+++ HW13/HW13_OrbitHW/HW13_OrbitHW/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace HW13_OrbitHW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.planet_radiobtn = new System.Windows.Forms.RadioButton();
+            this.gravity_Radiobtn = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 109);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(631, 546);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // planet_radiobtn
+            // 
+            this.planet_radiobtn.AutoSize = true;
+            this.planet_radiobtn.Location = new System.Drawing.Point(36, 86);
+            this.planet_radiobtn.Name = "planet_radiobtn";
+            this.planet_radiobtn.Size = new System.Drawing.Size(125, 17);
+            this.planet_radiobtn.TabIndex = 2;
+            this.planet_radiobtn.TabStop = true;
+            this.planet_radiobtn.Text = "Click to create planet";
+            this.planet_radiobtn.UseVisualStyleBackColor = true;
+            this.planet_radiobtn.CheckedChanged += new System.EventHandler(this.planet_radiobtn_CheckedChanged);
+            // 
+            // gravity_Radiobtn
+            // 
+            this.gravity_Radiobtn.AutoSize = true;
+            this.gravity_Radiobtn.Location = new System.Drawing.Point(36, 63);
+            this.gravity_Radiobtn.Name = "gravity_Radiobtn";
+            this.gravity_Radiobtn.Size = new System.Drawing.Size(235, 17);
+            this.gravity_Radiobtn.TabIndex = 1;
+            this.gravity_Radiobtn.TabStop = true;
+            this.gravity_Radiobtn.Text = "Click to create center of gravity (No Overlap)";
+            this.gravity_Radiobtn.UseVisualStyleBackColor = true;
+            this.gravity_Radiobtn.CheckedChanged += new System.EventHandler(this.gravity_Radiobtn_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Center of gravity radius (pixels):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Simulation Parameters";
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            this.Timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(201, 31);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(441, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 667);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gravity_Radiobtn);
+            this.Controls.Add(this.planet_radiobtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Ran Tao 11488080";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton planet_radiobtn;
+        private System.Windows.Forms.RadioButton gravity_Radiobtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ HW13/HW13_OrbitHW/HW13_OrbitHW/Form1.cs
@@ -0,0 +1,285 @@
+﻿/*******************************************************************************************
+* Programmer: Ran Tao
+* ID: 11488080
+* Class: CptS 321, Spring  2017
+* Programming Assignment: PA 13 - Orbiting Planets Animation
+* Created: April 18, 2017
+* Last Revised: April 21, 2017
+* Cited: MSDN documentation
+*******************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW13_OrbitHW
+{
+    public partial class Form1 : Form
+    {
+        List<Suns> suns;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            numericUpDown1.Value = 120;
+
+            suns = new List<Suns>();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            Timer.Enabled = true;
+            Timer.Interval = 20;
+            Timer.Start();
+        }
+        //glo var
+        private int mode = 0;
+
+        /*************************************************************
+        * Function: gravity_Radiobtn_CheckedChanged(object sender, EventArgs e)
+        * Date Created: April 18, 2017
+        * Date Last Modified: April 20, 2017
+        * Description: when checked gravity, mode become to 0;
+        * Return: NONE
+        *************************************************************/
+        private void gravity_Radiobtn_CheckedChanged(object sender, EventArgs e)
+        {
+            mode = 0;
+        }
+
+        /*************************************************************
+        * Function: planet_radiobtn_CheckedChanged(object sender, EventArgs e)
+        * Date Created: April 18, 2017
+        * Date Last Modified: April 20, 2017
+        * Description: when checked plante, mode become to 0;
+        * Return: NONE
+        *************************************************************/
+        private void planet_radiobtn_CheckedChanged(object sender, EventArgs e)
+        {
+            mode = 1;
+        }
+
+        /*************************************************************
+        * Function: pictureBox1_MouseClick(object sender, EventArgs e)
+        * Date Created: April 18, 2017
+        * Date Last Modified: April 20, 2017
+        * Description: click the picturebox, when start draw and record the data
+        * depends on which mode you are in.
+        * Return: NONE
+        *************************************************************/
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int gravity_redius = (int)numericUpDown1.Value;              
+
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                if (mode == 0)
+                {
+                    if (isTouch(e.X, e.Y, gravity_redius) == false)
+                    { 
+                        //darw the suns and space
+                        using (Brush b = new SolidBrush(ColorTranslator.FromHtml("#EFEFEF")))
+                        {
+                            g.FillEllipse(b,
+                                new RectangleF(e.X - gravity_redius,
+                                e.Y - gravity_redius,
+                                gravity_redius * 2,
+                                gravity_redius * 2));
+                        }
+                        // add sun into list
+                        Suns s = new Suns(e.X, e.Y, gravity_redius);
+
+                        if (s != null)
+                        {
+                            suns.Add(s);
+                        }
+                        //draw the block dot
+                        using (Brush b = new SolidBrush(Color.Black))
+                        {
+                            g.FillEllipse(b,
+                                new RectangleF(e.X - gravity_redius / 10,
+                                e.Y - gravity_redius / 10,
+                                gravity_redius / 5,
+                                gravity_redius / 5));
+                        }
+                    }
+                }
+                else // mode == 1
+                {
+                    Planets planet = new Planets(e.X, e.Y);
+
+                    if (planet != null)
+                    {
+                        using (Brush b = new SolidBrush(Color.Red))
+                        {
+                            //draw plante
+                            bool isOutside = true;
+                            foreach (Suns sun in suns)
+                            {
+                                if (Math.Sqrt(((sun.X - planet.x) * (sun.X - planet.x)) + ((sun.Y - planet.y) * (sun.Y - planet.y))) < sun.R - 10)
+                                {
+                                    //moveable plante
+                                    sun.planets.Add(planet);
+                                    break;
+                                }
+
+                                // is not outside
+                                if (Math.Sqrt(((sun.X - planet.x) * (sun.X - planet.x)) + ((sun.Y - planet.y) * (sun.Y - planet.y))) < sun.R + 10)
+                                {
+                                    isOutside = false;
+                                }
+                            }
+
+                            //avoid overlap with space
+                            if (isOutside == true)
+                            {
+                                g.FillEllipse(b, new RectangleF(e.X - 10f, e.Y - 10f, 20f, 20f));
+                            }
+                        }
+                    }
+                }
+                g.Dispose();
+
+                pictureBox1.Invalidate();
+            }
+        }
+
+        /*************************************************************
+        * Function: timer1_Tick(object sender, EventArgs e)
+        * Date Created: April 18, 2017
+        * Date Last Modified: April 20, 2017
+        * Description: using timer to redraw the picturebox
+        * Return: NONE
+        *************************************************************/
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                foreach (Suns s in suns)
+                {
+                    if (s != null)
+                    {
+                        //draw space
+                        using (Brush b = new SolidBrush(ColorTranslator.FromHtml("#EFEFEF")))
+                        {
+                            g.FillEllipse(b,
+                                new RectangleF((float)(s.X - s.R),
+                                (float)(s.Y - s.R),
+                                (float)s.R * 2,
+                                (float)s.R * 2));
+                        }
+
+                        //block dot
+                        using (Brush b = new SolidBrush(Color.Black))
+                        {
+                            g.FillEllipse(b,
+                                new RectangleF((float)(s.X - s.R / 10),
+                                (float)(s.Y - s.R / 10),
+                                (float)s.R / 5,
+                                (float)s.R / 5));
+                        }
+                    }
+
+                    foreach (Planets p in s.planets)
+                    {
+                        if (p != null)
+                        {
+                            //calc the xy and radians and cos
+                            using (Brush b = new SolidBrush(Color.Red))
+                            {
+                                double degrees = 1f;
+                                double radians = degrees * (Math.PI / 180f);
+                                double radius = Math.Sqrt((p.x - s.X) * (p.x - s.X) + (p.y - s.Y) * (p.y - s.Y));
+                                double cosTheta = Math.Cos(radians);
+                                double sinTheta = Math.Sin(radians);
+                                double X = (double)(cosTheta * (p.x - s.X) - sinTheta * (p.y - s.Y) + s.X);
+                                double Y = (double)(sinTheta * (p.x - s.X) + cosTheta * (p.y - s.Y) + s.Y);
+
+                                p.x = X;
+                                p.y = Y;
+
+                                //draw plante
+                                g.FillEllipse(b, new RectangleF((float)p.x - 10f, (float)p.y - 10f, 20f, 20f));
+                            }
+                        }
+                    }
+                }
+                //Dispose the grapics object
+                g.Dispose();
+                //tell the picturebox that it needs updating
+                pictureBox1.Invalidate();
+            }
+        }
+
+        /*************************************************************
+        * Function: isTouch(int x, int y)
+        * Date Created: April 18, 2017
+        * Date Last Modified: April 20, 2017
+        * Description: each gravity can't touch each other.
+        * Return: bool
+        *************************************************************/
+        private bool isTouch(int x, int y, int gravity_redius)
+        {
+            foreach (Suns sun in suns)
+            {   
+                if (Math.Sqrt(((sun.X - x) * (sun.X - x)) + ((sun.Y - y) * (sun.Y - y))) < (sun.R + gravity_redius))
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    //sun class
+    public class Suns
+    {
+        private double x;
+        private double y;
+        private double r;
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public double R
+        {
+            get { return r; }
+            set { r = value; }
+        }
+
+        public List<Planets> planets;
+
+        public Suns(double x, double y, double r)
+        {
+            X = x;
+            Y = y;
+            R = r;
+            planets = new List<Planets>();
+        }
+    }
+
+    //plante class.
+    public class Planets
+    {
+        public double x;
+        public double y;
+        public Planets(double X, double Y)
+        {
+            x = X;
+            y = Y;
+        }
+    }
+}
