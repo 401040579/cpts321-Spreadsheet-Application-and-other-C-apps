b3d2fb588550b54afcdc4eea301d8a7605cc69df
Ran Tao <ryantao96@gmail.com>
2018-06-15 11:03:03 -0700

    Add files via upload

---
+++ HW12/HW12_Threading_WinForm/HW12_Threading_WinForm/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace HW12_Threading_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sorting_btn = new System.Windows.Forms.Button();
+            this.sorting_textbox = new System.Windows.Forms.TextBox();
+            this.URL_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.download_btn = new System.Windows.Forms.Button();
+            this.download_textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sorting_btn
+            // 
+            this.sorting_btn.Location = new System.Drawing.Point(13, 13);
+            this.sorting_btn.Name = "sorting_btn";
+            this.sorting_btn.Size = new System.Drawing.Size(409, 23);
+            this.sorting_btn.TabIndex = 0;
+            this.sorting_btn.Text = "Go (sorting)";
+            this.sorting_btn.UseVisualStyleBackColor = true;
+            this.sorting_btn.Click += new System.EventHandler(this.sorting_btn_Click);
+            // 
+            // sorting_textbox
+            // 
+            this.sorting_textbox.Location = new System.Drawing.Point(13, 43);
+            this.sorting_textbox.Multiline = true;
+            this.sorting_textbox.Name = "sorting_textbox";
+            this.sorting_textbox.Size = new System.Drawing.Size(412, 574);
+            this.sorting_textbox.TabIndex = 1;
+            // 
+            // URL_textBox
+            // 
+            this.URL_textBox.Location = new System.Drawing.Point(431, 25);
+            this.URL_textBox.Name = "URL_textBox";
+            this.URL_textBox.Size = new System.Drawing.Size(558, 20);
+            this.URL_textBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(428, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL";
+            // 
+            // download_btn
+            // 
+            this.download_btn.Location = new System.Drawing.Point(431, 51);
+            this.download_btn.Name = "download_btn";
+            this.download_btn.Size = new System.Drawing.Size(558, 23);
+            this.download_btn.TabIndex = 4;
+            this.download_btn.Text = "Go (download string from URL)";
+            this.download_btn.UseVisualStyleBackColor = true;
+            this.download_btn.Click += new System.EventHandler(this.download_btn_Click);
+            // 
+            // download_textBox
+            // 
+            this.download_textBox.Location = new System.Drawing.Point(431, 80);
+            this.download_textBox.Multiline = true;
+            this.download_textBox.Name = "download_textBox";
+            this.download_textBox.Size = new System.Drawing.Size(558, 537);
+            this.download_textBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 629);
+            this.Controls.Add(this.download_textBox);
+            this.Controls.Add(this.download_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.URL_textBox);
+            this.Controls.Add(this.sorting_textbox);
+            this.Controls.Add(this.sorting_btn);
+            this.Name = "Form1";
+            this.Text = "Ran Tao 11488080";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sorting_btn;
+        private System.Windows.Forms.TextBox sorting_textbox;
+        private System.Windows.Forms.TextBox URL_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button download_btn;
+        private System.Windows.Forms.TextBox download_textBox;
+    }
+}
+
---
+++ HW12/HW12_Threading_WinForm/HW12_Threading_WinForm/Form1.cs
@@ -0,0 +1,324 @@
+﻿/*******************************************************************************************
+* Programmer: Ran Tao
+* ID: 11488080
+* Class: CptS 321, Spring  2017
+* Programming Assignment: PA 12 - Threading
+* Created: April 12, 2017
+* Last Revised: April 14, 2017
+* Cited: MSDN documentation
+*******************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW12_Threading_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            URL_textBox.Text = "http://www.google.com";
+        }
+
+        // globe vars
+        string url, result;
+        List<List<int>> single_thread_list = new List<List<int>>();
+        List<List<int>> mutil_thread_list = new List<List<int>>();
+        public delegate void DoDelegate(object o);
+
+        /*************************************************************
+        * Function: download_btn_Click(object sender, EventArgs e)
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 13, 2017
+        * Description: Click the download button
+        * Return: NONE
+        *************************************************************/
+        private void download_btn_Click(object sender, EventArgs e)
+        {
+            download_textBox.Clear();
+            download_textBox.Text = "[NOTICE] IF THE WEB STRING IS TOOOOO LONG, SETTING THE TEXT WILL CAUSE LAGGING.\r\n\r\n";
+            url = URL_textBox.Text;
+
+            isEnabled_download(false);
+
+            Thread download = new Thread(download_Web);
+            download.Start();
+        }
+
+        /*************************************************************
+        * Function: download_Web()
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 13, 2017
+        * Description: do the downloading thread
+        * Return: NONE
+        *************************************************************/
+        void download_Web()
+        {
+            if (url.Length <= 7)
+            {
+                result = "Please enter http:// at the front.";
+            }
+            else if (url.Substring(0, 7) == "http://")
+            {
+                using (var webClient = new System.Net.WebClient())     
+                {
+                    try
+                    {
+                        result = webClient.DownloadString(url);
+                    }
+                    catch
+                    {
+                        result = "The website is not responding, plz re-enter url.";
+                    }
+                }
+            }
+            else
+            {
+                result = "Please enter a valid URL - enter http:// at the front.";
+            }
+            
+            downloadOutput(result);
+            isEnabled_download(true);
+        }
+
+        /*************************************************************
+        * Function: isEnabled_download(bool bo)
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 13, 2017
+        * Description: set the display depend the downloading finshing or not
+        * Return: NONE
+        *************************************************************/
+        private void isEnabled_download(bool bo)
+        {
+            if (download_btn.InvokeRequired || URL_textBox.InvokeRequired || download_textBox.InvokeRequired)
+            {
+                download_btn.Invoke(new MethodInvoker(() => isEnabled_download(bo)));
+                URL_textBox.Invoke(new MethodInvoker(() => isEnabled_download(bo)));
+                download_textBox.Invoke(new MethodInvoker(() => isEnabled_download(bo)));
+            }
+            else
+            {
+                download_btn.Enabled = bo;
+                URL_textBox.Enabled = bo;
+                download_textBox.Enabled = bo;
+            }
+        }
+
+        /*************************************************************
+        * Function: downloadOutput(object obj)
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 13, 2017
+        * Description: invoke the message
+        * Return: NONE
+        *************************************************************/
+        private void downloadOutput(object obj)
+        {
+            if (InvokeRequired)
+            {
+                DoDelegate method = new DoDelegate(downloadOutput);
+                Invoke(method, obj);
+                return;
+            }
+
+            string text = (string)obj;
+            download_textBox.Text += text + "\r\n";
+        }
+
+        /*************************************************************
+        * Function: sorting_btn_Click(object sender, EventArgs e)
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 13, 2017
+        * Description: click the sort button
+        * Return: NONE
+        *************************************************************/
+        private void sorting_btn_Click(object sender, EventArgs e)
+        {
+            sorting_textbox.Clear();
+            isEnabled_sort(false);
+            single_thread_list = new List<List<int>>();
+            mutil_thread_list = new List<List<int>>();
+
+            Thread sort = new Thread(sortingList);
+            sort.Start();
+        }
+
+        /*************************************************************
+        * Function: sortingList(object o)
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 14, 2017
+        * Description: do the sorting thread
+        * Return: NONE
+        *************************************************************/
+        void sortingList(object o)
+        {
+            DoDelegate sort = new DoDelegate(sortingOutput);
+
+            Random randnum = new Random();
+
+            for (int x = 0; x < 8; x++)
+            {
+                List<int> sublist = new List<int>();
+                List<int> sublist2 = new List<int>();
+                for (int y = 0; y < 1000000; y++)
+                {
+                    sublist.Add(randnum.Next(0, 1000000));
+                    sublist2.Add(randnum.Next(0, 1000000));
+                }
+
+                single_thread_list.Add(sublist);
+                mutil_thread_list.Add(sublist2);
+            }
+
+            DateTime start = DateTime.Now;
+
+            foreach (var sub in single_thread_list)
+            {
+                sub.Sort();
+            }
+            DateTime end = DateTime.Now;
+
+            string singleThread = "Single-threaded time: " + (int)timeDiff(start, end) + " ms";
+            sort.Invoke(singleThread);
+
+            Thread multisorting = new Thread(multiSort);
+            multisorting.Start();
+        }
+
+        /*************************************************************
+        * Function: multiSort()
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 14, 2017
+        * Description: multisorting thread
+        * Return: NONE
+        *************************************************************/
+        private void multiSort()
+        {
+            DoDelegate multisorting = new DoDelegate(sortingOutput);
+            List<Thread> list_threads = new List<Thread>();
+
+            DateTime start = DateTime.Now;
+            
+            for (int i = 0; i < 8; i++)
+            {
+                // using ParameterizedThreadStart is a way to reliably get a sorted list
+                Thread t = new Thread(new ParameterizedThreadStart(sorti));
+                list_threads.Add(t);
+                t.Start(i);
+            }
+
+            //if put end here is roughly 4 (4 core cpu) time faster than the single.
+            //making interface more reasonable. and it turely sort all list.
+            DateTime end = DateTime.Now;
+            
+            //it reliably do all 8 threads.
+            foreach (Thread t in list_threads)
+            {
+                t.Join();
+            }
+
+            //DateTime end = DateTime.Now;
+            //but if put end here the time will increase, since join will pause the main thread.
+            //make sure all thread are done. but it will cause more time to do. and only less item may
+            //unsorted at this point. we may assume the list already sorted.
+
+            //determine when 8 threads complete
+            int count = 0;
+            foreach (var sub in mutil_thread_list)
+            {
+                count++;
+                for (int i = 0; i < sub.Count - 1; i++)
+                {
+                    if (sub[i] > sub[i + 1])
+                    {
+                        multisorting.Invoke("list " + count);
+                        multisorting.Invoke("not sorted " + sub[i] + " -> " + sub[i+1]);
+                    }
+                }
+            }
+
+            string MultiThread = "Multi-threaded time: " + (int)timeDiff(start, end) + " ms";
+            multisorting.Invoke(MultiThread);
+            isEnabled_sort(true);
+        }
+        
+        /*************************************************************
+        * Function: sorti(int i)
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 13, 2017
+        * Description: sort one list per thread
+        * Return: NONE
+        *************************************************************/
+        void sorti(object i)
+        {
+            mutil_thread_list[(int)i].Sort();
+        }
+
+        /*************************************************************
+        * Function: timeDiff(DateTime d1, DateTime d2)
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 13, 2017
+        * Description: cal the diff time
+        * Return: double
+        *************************************************************/
+        private double timeDiff(DateTime d1, DateTime d2)
+        {
+            double timeDiff = 0.0;
+            TimeSpan ts1 = new TimeSpan(d1.Ticks);
+            TimeSpan ts2 = new TimeSpan(d2.Ticks);
+            TimeSpan ts = ts1.Subtract(ts2).Duration();
+            timeDiff = ts.TotalMilliseconds;
+            return timeDiff;
+        }      
+
+        /*************************************************************
+        * Function: sortingOutput(object obj)
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 13, 2017
+        * Description: invoke sorting output
+        * Return: NONE
+        *************************************************************/
+        private void sortingOutput(object obj)
+        {
+            if (InvokeRequired)
+            {
+                DoDelegate method = new DoDelegate(sortingOutput);
+
+                Invoke(method, obj);
+
+                return;
+            }
+
+            string text = obj.ToString();
+            sorting_textbox.Text += text + "\r\n";
+        }
+
+        /*************************************************************
+        * Function: isEnabled_sort(bool bo)
+        * Date Created: April 12, 2017
+        * Date Last Modified: April 13, 2017
+        * Description: set the display depend the sorting finshing or not
+        * Return: NONE
+        *************************************************************/
+        private void isEnabled_sort(bool bo)
+        {
+            if (sorting_btn.InvokeRequired || sorting_textbox.InvokeRequired)
+            {
+                sorting_btn.Invoke(new MethodInvoker(() => isEnabled_sort(bo)));
+                sorting_textbox.Invoke(new MethodInvoker(() => isEnabled_sort(bo)));
+            }
+            else
+            {
+                sorting_btn.Enabled = bo;
+                sorting_textbox.Enabled = bo;
+            }
+        }
+    }
+}
